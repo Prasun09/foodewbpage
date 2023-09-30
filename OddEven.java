5041cfafde56bc98cfb50141acc1a1a12c2e4475
Prasun <132467019+Prasun09@users.noreply.github.com>
2023-10-01 01:14:01 +0530

    Update OddEven.java
    please accept mine

--- OddEven.java
+++ OddEven.java
@@ -1,7 +1,15 @@
+// we are using package to take input from user
+import java.util.Scanner;
 public class OddEven {
     public static void main(String args[]) {
-        int n = 10;
-        int bitMask = 1;
+        Scanner sc=new Scanner (System.in);
+        // Promptimg  the user for input
+        System.out.print("Enter an integer: ");
+
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int bitMask = 1;
+            }
         if((n & bitMask) == 0) {
             System.out.println("even");
         } else {
